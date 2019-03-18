dcf423b82309e6840999930254dce41624f88cc6
Jamie <jamielammas1@gmail.com>
2019-03-18 00:21:47 +0000

    react sample app structure
    added docker
    added productlist component 
    added detail component
    some styling

---
+++ src/components/Detail.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class Detail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      item: null,
+    }
+  }
+  
+  componentDidMount() {
+    const { endpoint } =  this.props.location.state;
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            item: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error,
+          });
+        }
+      )
+  }
+  
+
+  render() {
+    const { error, isLoaded, item } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="Detail">
+          <div className="header">
+            <h1>Product</h1>
+          </div>
+          <div className="detail-container" key={item.id}>
+            <div className="big-image">
+              <img src={logo} />
+            </div>
+            <div className="title-container">
+                <h3>{item.name}</h3>
+                <p>{item.description}</p>
+                <a href={item.external_url}>{item.external_url}</a>
+                <p>Votes: {item.votes}</p>
+                <p>Published by: {item.user}</p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+export default Detail;
---
+++ src/components/ProductList.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+import logo from '../logo.svg';
+import '../App.css';
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    }
+  }
+  
+  componentDidMount() {
+    fetch("https://rideto-django-api.herokuapp.com/products")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error,
+          });
+        }
+      )
+  }
+  
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="Detail">
+          <div className="header">
+              <h1>Products</h1>
+            </div>
+            
+            {items.map(item => (
+              <Link key={item.id} to={{ pathname: "/detail", state: { endpoint: item.url } }} >
+                <div  className="container" >
+                  <div className="thumbnail">
+                    <img src={logo} alt=" "/>
+                  </div>
+                  <div className="title-container">
+                      <h3>{item.name}</h3>
+                      <p>{item.description}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+        </div>
+      );
+    }
+  }
+}
+
+export default ProductList;
